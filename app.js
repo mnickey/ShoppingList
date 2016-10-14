509ee61c5149aa7f9647d0b5c26df571b08c13bc
Michael Nickey <mnickey@gmail.com>
2016-10-14 00:05:23 -0700

    work on checking items toggleClass

--- app.js
+++ app.js
@@ -8,26 +8,34 @@ var state = {
 };
 
 // state modification functions
-var addItem = function(state, item) {
+function addItem(state, item) {
     state.items.push(item);
-};
+    console.log(state.items);
+}
 
 // Render functions
-var renderList = function(state, element) {
-    var itemsHTML = state.items.map(function(item) {
-        return '<li>' + item + '</li>';
+function renderList(state, element) {
+    var itemsHTML = state.items.map(function (item) {
+        var addedItem = '<li>' + '<span class="shopping-item">' + item + '</span>';
+        addedItem += '<div class="shopping-item-controls">' + '<button class="shopping-item-toggle">' +
+            '<span class="button-label">check</span>' + '</button>' +
+            '<button class="shopping-item-delete">' + '<span class="button-label">delete</span>' +
+                '</button>' + '</div>' + '</li>';
+        // console.log(addedItem);
+        return addedItem;
     });
     element.html(itemsHTML);
-};
+}
 
-var checked = function(element) {
-    return checked.addClass('shopping-item__checked');
-};
+function checkItem(element) {
+    return $(this).closest('li').toggleClass(element);
+    // return ($(element).find('span').addClass('shopping-item__checked'));
+}
 
 // Event listeners
-$('.shopping-list-add').submit(function(event) {
+$('#js-shopping-list-form').submit(function (event) {
     event.preventDefault();
-    addItem(state, $('.shopping-list-add-input').val());
-    checkItem($('span').currentTarget);
+    addItem(state, $('#shopping-list-entry').val());
+    checkItem($('.shopping-list').text());
     renderList(state, $('.shopping-list'));
 });
