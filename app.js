335ed50b34e3916d2a0f1cb3a2ca274e74e07a28
Michael Nickey <mnickey@gmail.com>
2016-10-17 22:42:15 -0700

    updated addItem method to accept name & checked values

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ function addItem(state, item) {
 function checkItem(element) {
     // toggles the class shopping-item__checked when button inside the parent <li> is clicked
     element.closest('li').toggleClass('shopping-item__checked');
+    console.log(element.find('shopping-item').text());
 }
 
 // Render functions
